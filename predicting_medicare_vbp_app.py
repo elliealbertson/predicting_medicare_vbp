212d68d34737f3a35137e2483b1ab44d93385d8d
elliealbertson <120353163+elliealbertson@users.noreply.github.com>
2023-12-28 23:14:32 -0800

    Update code

--- predicting_medicare_vbp_app.py
+++ predicting_medicare_vbp_app.py
@@ -1,4 +1,7 @@
 import pickle
 import streamlit as st
 
+with open('predicting_medicare_vbp_pipeline.pkl', 'rb') as file:
+    pipeline = pickle.load(file)
+
 st.title('Predicting Medicare Value-Based Payment Program Participation')
